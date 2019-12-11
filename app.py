91b4377958d7744e3fc423eea3111d263b0afbd3
unknown <trung3t123@gmail.com>
2019-12-11 22:44:38 +0700

    show 5

--- app.py
+++ app.py
@@ -72,6 +72,8 @@ def showquestion():
 @app.route('/API/show5Question', methods = ['GET'])
 def show5Question():
     question = Question.query.order_by(func.random()).limit(5).all()
+    result = questions_schema.jsonify(question)
+    return result
 #uninstalled unusing package
 # asdaDSdsafaf
 
